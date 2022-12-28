ef43f2d2097e83d2fc4a84b88c50e75445a823c2
arvindholmes-asimo <arvind.holmes@gmail.com>
2022-12-28 16:07:22 +0530

    update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ $(document).ready(function () {
   $("#banner_carousel").on("beforeChange", function () {
 
     $('.caption').removeClass('animated fadeInLeft').hide();
-      $('.caption').addClass('animated fadeInLeft').show();
+    $('.caption').addClass('animated fadeInLeft').show();
 
 
   })
@@ -161,3 +161,43 @@ $(document).ready(function () {
   });
 })
 
+
+// find more button 
+const btnTarget = document.querySelector("#what_we_offer .grid-item:last-child");
+const findOutMoreTemplate = `<button class="more-btn">Find out more</button>`;
+btnTarget.insertAdjacentHTML("afterbegin", findOutMoreTemplate);
+
+
+// chatbot 
+const footerEl = document.querySelector('.footer-container');
+const chatbot = document.querySelector('.chatbot');
+const banner = document.querySelector('#banner_carousel');
+
+function isInViewport(ele) {
+  const { top, bottom } = ele.getBoundingClientRect();
+  const vHeight = (window.innerHeight || document.documentElement.clientHeight);
+
+  return (
+    (top > 0 || bottom > 0) &&
+    top < vHeight
+  );
+}
+
+document.addEventListener("scroll", (event) => {
+  const status = isInViewport(footerEl);
+  console.log(status);
+  if (status) {
+    let pos = window.innerHeight - footerEl.getBoundingClientRect().y;
+    console.log(footerEl.getBoundingClientRect().y - chatbot.getBoundingClientRect().height)
+    chatbot.style.cssText = `top: ${footerEl.getBoundingClientRect().y - chatbot.getBoundingClientRect().height - 10}px;transform:none;`;
+  }
+  else {
+    chatbot.style.cssText = `top: ${banner.getBoundingClientRect().height - 80}px; transform: none;`;
+  }
+
+})
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  chatbot.style.cssText = `top: ${banner.getBoundingClientRect().height - 80}px; transform: none;`;
+});
--- timelinebtn.js
+++
@@ -1,3 +0,0 @@
-const btnTarget = document.querySelector("#what_we_offer .grid-item:last-child");
-const findOutMoreTemplate = `<button class="more-btn">Find out more</button>`;
-btnTarget.insertAdjacentHTML("afterbegin", findOutMoreTemplate);
