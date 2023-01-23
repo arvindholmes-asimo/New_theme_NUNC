6a4686c089e8edb960896a27010031e5e34d2fe5
arvindholmes-asimo <arvind.holmes@gmail.com>
2023-01-23 19:21:45 +0530

    navbar-responsive

--- script.js
+++ script.js
@@ -230,12 +230,3 @@ document.addEventListener("DOMContentLoaded", () => {
   chatbot.style.cssText = `top: ${banner.getBoundingClientRect().height - 100}px; transform: none;`;
 });
 
-
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
