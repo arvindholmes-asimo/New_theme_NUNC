459348aec3e6764f92969947120276025811e425
arvindholmes@asimo <arvind.holmes@gmail.com>
2023-08-03 08:55:54 +0530

    editing_pages

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
-
   // banner carousel script
-  $('#banner_carousel').slick({
+  $("#banner_carousel").slick({
     adaptiveHeight: false,
     variableWidth: false,
     centerPadding: 0,
@@ -17,8 +16,8 @@ $(document).ready(function () {
           slidesToShow: 1,
           slidesToScroll: 1,
           infinite: true,
-          dots: true
-        }
+          dots: true,
+        },
       },
       {
         breakpoint: 1024,
@@ -26,29 +25,26 @@ $(document).ready(function () {
           slidesToShow: 1,
           slidesToScroll: 1,
           infinite: true,
-          dots: true
-        }
+          dots: true,
+        },
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 1,
           dots: true,
-          slidesToScroll: 1
-        }
+          slidesToScroll: 1,
+        },
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-
+          slidesToScroll: 1,
+        },
+      },
     ],
   });
-
-
 
   // if ($('.slick-slide').hasClass('slick-active')) {
   //   $('.caption').addClass('animated fadeInLeft');
@@ -61,12 +57,14 @@ $(document).ready(function () {
   //   $('.caption').removeClass('animated fadeInLeft').hide();
   //   $('.caption').addClass('animated fadeInLeft').show();
 
-
   // })
-  
-  let headingAnimate = 'animate__animated animate__slideInUp animate__delay-0.1s';
-  let tagLineAnimate = 'animate__animated animate__slideInUp animate__delay-.4s';
-  let readmoreAnimate = 'animate__animated animate__slideInUp animate__delay-.6s';
+
+  let headingAnimate =
+    "animate__animated animate__slideInUp animate__delay-0.1s";
+  let tagLineAnimate =
+    "animate__animated animate__slideInUp animate__delay-.4s";
+  let readmoreAnimate =
+    "animate__animated animate__slideInUp animate__delay-.6s";
   $("#banner_carousel").on("afterChange", function (e, slick, currentSlide) {
     console.log({ slick, currentSlide });
     $(".slick-slide .heading1").removeClass(headingAnimate);
@@ -75,19 +73,17 @@ $(document).ready(function () {
     $(".slick-current .heading1").addClass(headingAnimate);
     $(".slick-current .tag-line").addClass(tagLineAnimate);
     $(".slick-current .read_more").addClass(readmoreAnimate);
-  
-  })
+  });
 
-  $('#banner_carousel').on('init', function(event, slick){
+  $("#banner_carousel").on("init", function (event, slick) {
     console.log("initialized");
     $(".slick-current .heading1").addClass(headingAnimate);
     $(".slick-current .tag-line").addClass(tagLineAnimate);
     $(".slick-current .read_more").addClass(readmoreAnimate);
     // event subscriber goes here
-});
+  });
   // client carousel script
-  $('#client_carousel').slick({
-
+  $("#client_carousel").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: false,
@@ -103,8 +99,8 @@ $(document).ready(function () {
           slidesToScroll: 1,
           infinite: true,
           dots: true,
-          speed: 1000
-        }
+          speed: 1000,
+        },
       },
       {
         breakpoint: 1024,
@@ -113,38 +109,36 @@ $(document).ready(function () {
           slidesToScroll: 1,
           infinite: true,
           dots: true,
-          speed: 1000
-        }
+          speed: 1000,
+        },
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 1,
           arrows: true,
-          slidesToScroll: 1
-        }
+          slidesToScroll: 1,
+        },
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          arrows:true,
-          slidesToScroll: 1
-        }
-      }
-
+          arrows: true,
+          slidesToScroll: 1,
+        },
+      },
     ],
   });
 
   //case_studies_carousel_script
-  $('#case_studies_carousel').slick({
-
+  $("#case_studies_carousel").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 5000,
     arrows: true,
-    mobileFirst:true,
+    mobileFirst: true,
     responsive: [
       {
         breakpoint: 1400,
@@ -152,60 +146,52 @@ $(document).ready(function () {
           slidesToShow: 1,
           slidesToScroll: 1,
           infinite: true,
-          arrows:true,
+          arrows: true,
           dots: true,
-          speed: 1000
-        }
+          speed: 1000,
+        },
       },
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          arrows:true,
+          arrows: true,
           infinite: true,
           dots: true,
-          speed: 1000
-        }
+          speed: 1000,
+        },
       },
       {
         breakpoint: 600,
         arrows: true,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
+          slidesToScroll: 1,
+        },
       },
       {
         breakpoint: 480,
         arrows: true,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-
+          slidesToScroll: 1,
+        },
+      },
     ],
   });
-})
+});
 
-
-
-
-
-// chatbot 
-const footerEl = document.querySelector('.footer-container');
-const chatbot = document.querySelector('.chatbot');
-const banner = document.querySelector('.hero-container');
+// chatbot
+const footerEl = document.querySelector(".footer-container");
+const chatbot = document.querySelector(".chatbot");
+const banner = document.querySelector(".hero-container");
 
 function isInViewport(ele) {
   const { top, bottom } = ele.getBoundingClientRect();
-  const vHeight = (window.innerHeight || document.documentElement.clientHeight);
+  const vHeight = window.innerHeight || document.documentElement.clientHeight;
 
-  return (
-    (top > 0 || bottom > 0) &&
-    top < vHeight
-  );
+  return (top > 0 || bottom > 0) && top < vHeight;
 }
 
 document.addEventListener("scroll", (event) => {
@@ -213,35 +199,40 @@ document.addEventListener("scroll", (event) => {
   console.log(status);
   if (status) {
     let pos = window.innerHeight - footerEl.getBoundingClientRect().y;
-    console.log(footerEl.getBoundingClientRect().y - chatbot.getBoundingClientRect().height)
-    chatbot.style.cssText = `top: ${footerEl.getBoundingClientRect().y - chatbot.getBoundingClientRect().height - 10}px;transform:none;`;
+    console.log(
+      footerEl.getBoundingClientRect().y -
+        chatbot.getBoundingClientRect().height
+    );
+    chatbot.style.cssText = `top: ${
+      footerEl.getBoundingClientRect().y -
+      chatbot.getBoundingClientRect().height -
+      10
+    }px;transform:none;`;
+  } else {
+    chatbot.style.cssText = `top: ${
+      banner.getBoundingClientRect().height - 100
+    }px; transform: none;`;
   }
-  else {
-    chatbot.style.cssText = `top: ${banner.getBoundingClientRect().height - 100}px; transform: none;`;
-  }
-
-})
-
-
-document.addEventListener("DOMContentLoaded", () => {
-  chatbot.style.cssText = `top: ${banner.getBoundingClientRect().height - 100}px; transform: none;`;
 });
 
-let menu = document.getElementById('hamberger-menu');
-let headerbar = document.querySelector('.headerbar');
-menu.addEventListener('toggle', openNav);
+document.addEventListener("DOMContentLoaded", () => {
+  chatbot.style.cssText = `top: ${
+    banner.getBoundingClientRect().height - 100
+  }px; transform: none;`;
+});
+
+let menu = document.getElementById("hamberger-menu");
+let headerbar = document.querySelector(".headerbar");
+menu.addEventListener("toggle", openNav);
 // action
 function openNav() {
-    if (headerbar.style.display === 'none') {
-        headerbar.style.display = 'inline-block';
-        headerbar.style.transform = "translateY:65%";
-    }
-    else {
-        headerbar.style.display = "none"
-    }
-
+  if (headerbar.style.display === "none") {
+    headerbar.style.display = "inline-block";
+    headerbar.style.transform = "translateY:65%";
+  } else {
+    headerbar.style.display = "none";
+  }
 }
 
-
-const year = document.getElementById('year');
+const year = document.getElementById("year");
 year.textContent = new Date().getFullYear();
